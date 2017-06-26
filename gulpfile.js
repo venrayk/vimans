7afccb05d0257fb8207c58661a34ba3a2913bd54
2469415004@qq.com <wang163lk>
2017-06-27 00:08:56 +0800

    change gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
- //引入gulp
+//引入gulp
 var gulp = require('gulp');
 
 //引入webserver插件
-var webserver = require('gulp-webserver'); 
+var webserver = require('gulp-webserver');
 
-gulp.task('webserver', function(){
-gulp.src('./')
-.pipe(webserver({
-port: 3000,//端口
-host: '127.0.0.1',//域名
-liveload: true,//实时刷新代码。不用f5刷新
-directoryListing: {
-path: './index.html',
-enable: true
-}
-}))
+gulp.task('default', function () {
+    gulp.src('./')
+        .pipe(webserver({
+            port: 80,//端口
+            // host: '127.0.0.1',//域名
+            host: '10.135.54.30',//域名
+            liveload: true,//实时刷新代码。不用f5刷新
+            directoryListing: {
+                path: './index.html',
+                enable: true
+            }
+        }))
 });
