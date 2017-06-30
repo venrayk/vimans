528858e816ec714c5c5c3fca2c5f976107ee307d
“venrayk” <venrayk@163.com>
2017-06-30 20:55:23 +0800

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,15 @@
+
 //引入gulp
 var gulp = require('gulp');
 
 //引入webserver插件
 var webserver = require('gulp-webserver');
 
-gulp.task('default', function () {
-    gulp.src('./')
-        .pipe(webserver({
-            port: 80,//端口
-            // host: '127.0.0.1',//域名
-            host: '10.135.54.30',//域名
-            liveload: true,//实时刷新代码。不用f5刷新
-            directoryListing: {
-                path: './index.html',
-                enable: true
-            }
-        }))
+gulp.task('default', function() {
+  gulp.src('./')
+    .pipe(webserver({
+  	host:'0.0.0.0',
+	port:'80',
+	fallback: 'index.html'
+    }));
 });
